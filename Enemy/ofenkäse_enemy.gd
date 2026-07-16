e6b941d876ba06a5a4e298c449a941a922a527b5
LorisSuperstar <180442737+LorisSuperstar@users.noreply.github.com>
2026-07-16 10:57:48 +0200

    add direction change

--- Enemy/ofenkäse_enemy.gd
+++ Enemy/ofenkäse_enemy.gd
@@ -26,3 +26,8 @@ func _on_area_2d_body_entered(body: Node2D) -> void:
 	if body.name == "Player":
 		print("The Player jumped on me!")
 		queue_free()
+
+
+func _on_timer_timeout() -> void:
+	print("turn around")
+	is_going_right = !is_going_right
--- Items/monster.gd
+++ Items/monster.gd
@@ -26,3 +26,8 @@ func _on_area_2d_body_entered(body: Node2D) -> void:
 	if body.has_method("make_normal_size"):
 		body.make_normal_size()
 		queue_free()
+
+
+func _on_timer_timeout() -> void:
+	print("turn around")
+	is_going_right = !is_going_right
