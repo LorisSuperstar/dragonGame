b4a5c98966ba061d1d823f9c9136052cdd1a238d
LorisSuperstar <180442737+LorisSuperstar@users.noreply.github.com>
2026-07-16 09:49:02 +0200

    add crates

---
+++ Block/question_block.gd
@@ -0,0 +1,16 @@
+extends StaticBody2D
+
+@export var spawn_item_scene: PackedScene
+@export var scale_x: float
+@export var scale_y: float
+@onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
+
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	
+	animated_sprite.play("open")
+	var item = spawn_item_scene.instantiate()
+	item.scale = Vector2(scale_x, scale_y)
+	item.position = global_position + Vector2(0, -16)
+	get_parent().add_child(item)
+	queue_free()
+	
