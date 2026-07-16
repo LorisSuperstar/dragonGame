703ec72b8d24f021fbaa147be264fac346772305
LorisSuperstar <180442737+LorisSuperstar@users.noreply.github.com>
2026-07-16 11:51:45 +0200

    fix circular dependency

--- Goal/goal.gd
+++ Goal/goal.gd
@@ -1,8 +1,10 @@
 extends Area2D
 
-@export var next_level_scene: PackedScene
+# 1. Change PackedScene to a String file path
+@export_file("*.tscn") var next_level_scene: String
 
 func _on_body_entered(body: Node2D) -> void:
-	if body.name.to_lower().begins_with("player"):
-		print("Level Completed!")
-		get_tree().change_scene_to_packed(next_level_scene)
+    if body.name.to_lower().begins_with("player"):
+        print("Level Completed!")
+        # 2. Change change_scene_to_packed to change_scene_to_file
+        get_tree().change_scene_to_file(next_level_scene)
