549004300d8beb56dad63bd0cf50a1d58d242899
LorisSuperstar <180442737+LorisSuperstar@users.noreply.github.com>
2026-07-17 15:20:08 +0200

    add checkpoint

--- Block/check_point.gd
+++ Block/check_point.gd
@@ -7,3 +7,4 @@ func _on_body_entered(body: Node2D) -> void:
 	if body.name == "Player" and not activated:
 		activated = true
 		body.global_position_checkpoint = global_position
+		$Checkpoint.play()
